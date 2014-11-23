c52774d260b7a1cc8c786ab9cbea8817a37609d1
ed <ejbyne@gmail.com>
2014-11-23 00:10:53 +0000

    added tests for checking correct username and password

--- features/log_in.feature
+++ features/log_in.feature
@@ -11,3 +11,13 @@ Scenario: Logging in
   Given I click the "Sign In" button on the homepage
   When I enter my sign-in details and click "Sign In"
   Then I should see "Welcome, roger_t"
+
+Scenario: I can't log in with the incorrect username
+  Given I click the "Sign In" button on the homepage
+  When I enter an incorrect username and click "Sign In"
+  Then I should see "The username or password is incorrect"
+
+Scenario: I can't log in with the incorrect password
+  Given I click the "Sign In" button on the homepage
+  When I enter an incorrect password and click "Sign In"
+  Then I should see "The username or password is incorrect"
--- features/step_definitions/log_in_steps.rb
+++ features/step_definitions/log_in_steps.rb
@@ -11,3 +11,31 @@ When(/^I enter my sign\-in details and click "(.*?)"$/) do |arg1|
   fill_in "password", :with => "test"
   click_button(arg1)
 end
+
+When(/^I enter an incorrect username and click "(.*?)"$/) do |arg1|
+  User.create(  user_name:             "roger_t",
+                first_name:            "Roger",
+                last_name:             "Test",
+                email:                 "test@test.com",
+                email_confirmation:    "test@test.com",
+                password:              "test",
+                password_confirmation: "test")
+  visit '/sessions/new'
+  fill_in "user_name", :with => "roger_t2"
+  fill_in "password", :with => "test"
+  click_button(arg1)
+end
+
+When(/^I enter an incorrect password and click "(.*?)"$/) do |arg1|
+  User.create(  user_name:             "roger_t",
+                first_name:            "Roger",
+                last_name:             "Test",
+                email:                 "test@test.com",
+                email_confirmation:    "test@test.com",
+                password:              "test",
+                password_confirmation: "test")
+  visit '/sessions/new'
+  fill_in "user_name", :with => "roger_t"
+  fill_in "password", :with => "test2"
+  click_button(arg1)
+end
